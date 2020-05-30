4dfd700e4b8e5d6e0014c17c1e8e2f0b71ad85ee
DmitrySinitsin <sera@bankir.ru>
2020-05-30 10:29:16 +0400

    memoryCards

---
+++ MemoryCards/FormMemory.cs
@@ -0,0 +1,133 @@
+﻿using MemoryLibrary;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MemoryCards
+{
+    public partial class FormMemory : Form, IPlayable
+    {
+
+        LogicMemory logic;
+
+        public FormMemory()//конструктор
+        {
+            InitializeComponent();
+            logic = new LogicMemory(this);
+            //init_game();
+            logic.CreateNewGame();
+        }
+
+        private void menu_game_exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void munu_help_rules_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            @"Цель игры - открыть все карточки
+            за минимальное количество ходов.
+
+            На столе лежит 16 перевернутых карточек.
+            На них изображено 8 разных картинок.
+            Каждая картинка изображена дважды.
+            Необходимо найти парные карточки.
+
+            Щёлкайте по карточкам чтобы их перевернуть.
+            Если пара подобрана верно - карточки остаются.
+            Если ошибочно - карточки перевернутся назад.
+            Запоминайте картинки на карточках,
+            Чтобы в следующий раз открыть их верно. ", "Правила игры");
+        }
+
+        private void menu_help_about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            @"Игровая программа 'Карточки памяти'
+            создана в обучающих целях
+            на практическом видеокурсе
+            'Изучаем язык с C# нуля'.
+
+            http://wwwVideoSharp.info/", "О программе");
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int nr = int.Parse (((PictureBox)sender).Tag.ToString()); //возвращает в переменной nr номер нажатой картинки
+                                                                      //MessageBox.Show(nr.ToString()); -вывести диалог с номером тега картинки
+            logic.ClickPicture(nr);
+            //return;
+
+        }
+
+         private void LoadPicture (int picture, int image)
+        {
+            getPictureBox(picture).Image = getImage (image);
+        }
+
+        private PictureBox getPictureBox (int picture)
+        {
+            switch (picture)
+            {
+                case 0: return pictureBox0;
+                case 1: return pictureBox1;
+                case 2: return pictureBox2;
+                case 3: return pictureBox3;
+                case 4: return pictureBox4;
+                case 5: return pictureBox5;
+                case 6: return pictureBox6;
+                case 7: return pictureBox7;
+                case 8: return pictureBox8;
+                case 9: return pictureBox9;
+                case 10: return pictureBox10;
+                case 11: return pictureBox11;
+                case 12: return pictureBox12;
+                case 13: return pictureBox13;
+                case 14: return pictureBox14;
+                case 15: return pictureBox15;
+                default: return null;
+            }
+        }
+
+        private Image getImage (int image)
+        {
+            switch (image)
+            {
+                case 0: return Properties.Resources._0;
+                case 1: return Properties.Resources._1;
+                case 2: return Properties.Resources._2;
+                case 3: return Properties.Resources._3;
+                case 4: return Properties.Resources._4;
+                case 5: return Properties.Resources._5;
+                case 6: return Properties.Resources._6;
+                case 7: return Properties.Resources._7;
+                case 8: return Properties.Resources._8;
+                default: return null;
+            }
+        }
+
+        private void menu_game_new_Click(object sender, EventArgs e)
+        {
+            logic.CreateNewGame();
+        }
+
+        public void ShowCard (int nr, int card) //показать картинки, в метод передается номер картинки int picture
+        {
+            LoadPicture(nr, card); //загрузить картинку
+            getPictureBox(nr).Cursor = Cursors.Arrow;
+        }
+
+        public void HideCard (int picture)
+        {
+            LoadPicture(picture, 0);//показать нулевую картинку(спрятать)
+            getPictureBox(picture).Cursor = Cursors.Hand;
+        }
+
+        public void ShowWinner()
+        {
+            MessageBox.Show("Вы победили!", "Поздравляем");
+            //init_game();
+        }
+    }
+}
---
+++ MemoryLibrary/LogicMemory.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemoryLibrary
+{
+    public class LogicMemory
+    {
+        IPlayable play;
+        static Random rand = new Random();
+        int[] cards = new int[16];
+        bool[] opens = new bool[16];//булевский массив какие карточки уже открыты
+        int done;//сколько картинок открыто
+        int status = 0;//выбор типа действия программы в соответствии с блок-схемой
+        int card_a;// первая из открываемых в процессе игры карточек
+        int card_b;// вторая из открываемых в игре карточек
+
+
+        public LogicMemory(IPlayable play)
+        {
+            this.play = play;
+        }
+        public void CreateNewGame()
+        {
+            /*for (int j = 0; j < 16; j++)
+                play.ShowCard(j, j / 2 + 1);
+                    //play.HideCard(j);*/
+            for (int j = 0; j < cards.Length; j++)
+                cards[j] = j % (cards.Length / 2) + 1;//создаем картинки
+            for (int j = 0; j < 100; j++)
+                shuffle_cards();// перемешиваем картинки
+            /*for (int j = 0; j < cards.Length; j++)
+                load_picture(j, 0);//загружаем и спрятать все картинки через 0*/
+            for (int j = 0; j < cards.Length; j++)
+                play.HideCard(j);// спрятать все картинки методом hide
+            for (int j = 0; j < cards.Length; j++)
+                opens[j] = false;//все картинки массива opens с значением false то есть закрыты
+            done = 0;//открыто ноль картинок
+            status = 0;
+
+        }
+
+        public void ClickPicture(int nr)
+        {
+            /*play.ShowCard(nr, nr / 2 + 1);
+            if (nr == 15)
+                play.ShowWinner();*/
+            if (opens[nr]) return;
+            switch (status)
+            {
+                case 0: status_0(nr); break;
+                case 1: status_1(nr); break;
+                case 2: status_2(nr); break;
+                case 3: status_3(nr); break;
+            }
+
+        }
+
+        private void shuffle_cards()
+        {
+            int a = rand.Next(0, cards.Length);
+            int b = rand.Next(0, cards.Length);
+            if (a == b) return;
+            int x;
+            x = cards[a];
+            cards[a] = cards[b];
+            cards[b] = x;
+        }
+        private void open(int picture)//метод открывает картинку и запрещает её закрытие
+        {
+            opens[picture] = true;//картинка массива opens с номером picture true (открыта)
+            play.ShowCard(picture, cards[picture]);// метод show для номера picture - показать картинку
+        }
+
+        private void status_0(int nr)
+        {
+            card_a = nr;//какая была первая нажатая картинка
+            play.ShowCard(card_a, cards[card_a]);//показать кликнутую картинку
+            status = 1;//изменить статус с 0 на 1
+        }
+
+        private void status_1(int nr)
+        {
+            card_b = nr;//какая была вторая нажатая картинка
+            if (card_a == card_b)//если карточки одинаковы
+                return;//выйти из
+            play.ShowCard(card_b, cards[card_b]);
+            status = 2;
+            if (cards[card_a] == cards[card_b])
+            {
+                open(card_a);//открываем карточку а
+                open(card_b);//открываем карточку б
+                done += 2;// количество открытых карточек увеличить на 2
+                if (done == 16)//если счетчик равен 16 то есть открыты все карточки
+                    play.ShowWinner();// вызвать метод поздравления с победой
+                else
+                    status = 0;
+            }
+            else
+                status = 3;
+        }
+
+        private void status_2(int nr)
+        {
+            status = 0;
+        }
+
+        private void status_3(int nr)
+        {
+            play.HideCard(card_a);
+            play.HideCard(card_b);
+            status_0(nr);
+        }
+
+    }
+}
